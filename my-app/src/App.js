a02f481b6ea39f0f470cf9df5de0d3ae256f9545
Morgan Asare <ohenemorgan@gmail.com>
2024-06-28 09:06:15 +0000

    add navbar component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,15 @@
 import css from './App.module.css';
+import Sidebar from './components/Sidebar';
+import NavBarSimple from './components/NavBarSimple';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className={css.App}>
-      {/* Add your components here */}
+      <Sidebar/>
+      <NavBarSimple/>
+     
+      {/* <StatefulGreeting/> */}
     </div>
   );
 }
---
+++ my-app/src/components/NavBarSimple.js
@@ -0,0 +1,56 @@
+import css from './css/NavBarSimple.module.css'
+import React, { Component } from "react";
+
+class NavBarSimple extends Component {
+    constructor(){
+        super();
+        this.state = {
+            message: "Hello, guest!",
+            buttonText: "log in",
+        }
+    }
+
+    handleClick() {
+        this.setState((prevState, prevProps) =>{
+    
+            console.log('Previous State:', prevState)
+            console.log('Previous Props', prevProps)
+            return {
+                message: prevState.message === "Hello, guest!" ? "Welcome back, User!" : "Hello, guest!",
+                buttonText: prevState.buttonText === "log in" ? "log out" : "log in"
+            }
+            
+        }
+    );
+    
+    }
+
+    render() {
+
+        return <div className={css.NavBar}>
+            <h1>My Gallery</h1>
+
+            <div>
+                <span>{this.state.message}</span>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+            </div>
+
+           </div>
+    
+    }
+}
+
+
+// const NavBarSimple = () => {
+//     return <nav className='css.NavBar'>
+//             <h1>My Gallery</h1>
+
+//             <div>
+//                 <span></span>
+//                 <button></button>
+//             </div>
+
+//            </nav>
+// }
+
+export default NavBarSimple;
