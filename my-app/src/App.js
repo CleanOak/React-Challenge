526a0469cdf86d19b6ebbfca4ee1820815928f67
Morgan Asare <ohenemorgan@gmail.com>
2024-07-02 20:13:57 +0000

    add navbar example

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,15 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import NavBarSimple from './components/NavBarSimple';
-import StatefulGreeting from './components/StatefulGreeting';
-import ConditionalRenderingClass from './components/ConditionalRenderingClass';
-import ConditionalRenderingFunction from './components/ConditionalRenderingFunction';
+import NavBarForm from './components/NavBarForm';
+
 
 function App() {
   return (
     <div className={css.App}>
-      {/* <Sidebar/>
-      <NavBarSimple/> */}
-      {/* <StatefulGreeting/> */}
-        <ConditionalRenderingClass/>
-        <ConditionalRenderingFunction connected={false}/>
+      <Sidebar/>
+      {/* <NavBarSimple/>   */}
+      <NavBarForm/>
     </div>
   );
 }
