77d1dd5e49771b180ccf952f79b8130733e6936f
Morgan Asare <ohenemorgan@gmail.com>
2024-07-11 08:41:48 +0000

    Add use state with arrays

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import css from './App.module.css';
 
-import ControlledFormHooks from './components/ControlledFormHooks';
+import UseStateWithArrays from './components/UseStateWithArrays';
 
 
 function App() {
   return (
     <div className="App">
-      <ControlledFormHooks/>
+      <UseStateWithArrays/>
     </div>
   );
 }
