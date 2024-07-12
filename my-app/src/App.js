8171fbd954f7a67e250bb8e11bb4cd1b610dcdfb
Morgan Asare <ohenemorgan@gmail.com>
2024-07-12 10:04:42 +0000

    lesson on http requests

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,10 @@
-import css from './App.module.css';
-
-import UseStateWithArrays from './components/UseStateWithArrays';
+import HTTPRequests from "./components/HTTPRequests";
 
 
 function App() {
   return (
     <div className="App">
-      <UseStateWithArrays/>
+      <HTTPRequests/>
     </div>
   );
 }
